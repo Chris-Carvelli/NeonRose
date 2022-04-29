3d75411b3b9fc29712402a84690a2efa4d500952
Daniel <daniel@reepay.com>
2022-04-29 23:50:50 +0200

    Adds initial CharacterController

---
+++ Assets/Scripts/PlayerControllers/CharacterController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace NeonRose.PlayerControllers
+{
+    public class CharacterController : MonoBehaviour
+    {
+        public float speed = 5.0f;
+        private Rigidbody _body;
+        
+        public Vector3 _direction;
+
+        private void Start()
+        {
+            _body = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            _body.position += CalculateMovement();
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            var playerDirection = context.ReadValue<Vector2>();
+            _direction.x = playerDirection.x;
+            _direction.z = playerDirection.y;
+            _direction.y = 0;
+        }
+
+        private Vector3 CalculateMovement()
+        {
+            return _direction * speed;
+        }
+    }
+} 
--- Assets/Scripts/Temporary/SimpleCharacterController.cs
+++ Assets/Scripts/Temporary/SimpleCharacterController.cs
@@ -1,13 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SimpleCharacterController : MonoBehaviour
 {
+    public void move(InputAction.CallbackContext context)
+    {
+        Debug.Log("hit");
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Started");
     }
 
     // Update is called once per frame
