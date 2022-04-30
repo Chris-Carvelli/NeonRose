9e55df2a36d76c66afb796cd7407bd24e45da7b3
Chris <chris@modl.ai>
2022-04-30 22:31:03 +0200

    Spray Paint

---
+++ Assets/Packs/MeshPainter/TestBrush.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Packs.MeshPainter
+{
+    public class TestBrush : MonoBehaviour
+    {
+        public float radius = 1;
+        public float hardness = .5f;
+        public float strength = .5f;
+        public Color color = Color.red;
+        private void OnCollisionStay(Collision collision)
+        {
+            Paintable p = collision.transform.GetComponent<Paintable>();
+            if (p != null)
+            {
+                Vector3 pos = collision.contacts[0].point;
+                MeshPainterManager.Instance.Paint(p, pos, radius, hardness, strength, color);
+            }
+        }
+    }
+}
